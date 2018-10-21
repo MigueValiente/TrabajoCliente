41480da7c323a7df2ab91932763ae7b97106cb8b
MigueValiente <miguevaliente96@gmail.com>
2018-10-21 12:23:44 +0200

    Añadidas gran parte de las clases

---
+++ clases/videoclub.js
@@ -0,0 +1,32 @@
+class tienda{
+
+    constructor(nombre,libros,direccion){
+        this.nombre = nombre;
+        this.peliculas = peliculas;
+        this.direccion = direccion;
+    }
+
+    get nombre(){
+        return `${this._nombre}`;
+    }
+
+    set nombre(nombre){
+        this._nombre=nombre;
+    }
+
+    get peliculas(){
+        return `${this._peliculas}`;
+    }
+
+    set peliculas(peliculas){
+        this._peliculas=peliculas;
+    }
+
+    get direccion(){
+        return `${this._direccion}`;
+    }
+
+    set direccion(direccion){
+        this._direccion=direccion;
+    }
+}
---
+++ clases/votos.js
@@ -0,0 +1,43 @@
+class votos{
+
+    constructor(puntuacion,comentario,cliente,libro){
+        this.puntuacion = puntuacion;
+        this.comentario = comentario;
+        this.cliente = cliente;
+        this.pelicula = pelicula;
+    }
+
+    get puntuacion(){
+        return `${this._puntuacion}`;
+    }
+
+    set puntuacion(puntuacion){
+        this._puntuacion=puntuacion;
+    }
+
+    get comentario(){
+        return `${this._comentario}`;
+    }
+
+    set comentario(comentario){
+        this._comentario=comentario;
+    }
+
+    get cliente(){
+        return `${this._cliente}`;
+    }
+
+    set cliente(cliente){
+        this._cliente=cliente;
+    }
+
+    get pelicula(){
+        return `${this._pelicula}`;
+    }
+
+    set pelicula(pelicula){
+        this._pelicula=pelicula;
+    }
+
+
+}
